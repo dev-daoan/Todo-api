93b8644bed7a9cbc194ffa28291be0045d893c82
Dao An <langtudatinh117@gmail.com>
2016-11-22 10:57:15 +0700

    Add sequelize demo

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.get('/', function(req, res) {
 app.get('/todos', function(req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
-
     if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
         filteredTodos = _.where(filteredTodos, { completed: true });
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
